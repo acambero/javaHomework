901c6850713191907162ce95b2b297a2902e60a1
acambero <acambero@intellisys.com.do>
2013-05-08 11:09:58 -0800

    working with the test

--- src/codes/AccountHolder.java
+++ src/codes/AccountHolder.java
@@ -1,3 +1,26 @@
 package codes;
 public class AccountHolder {
+    private int balanceAllAccount;
+    private Account account;
+    private int balance;
+
+    public int getBalanceAllAccount() {
+        return balanceAllAccount;
+    }
+
+    public boolean addBalance(int amount$1000) {
+        return false;  //To change body of created methods use File | Settings | File Templates.
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public boolean withdrawMoney(int amount$200) {
+        return false;  //To change body of created methods use File | Settings | File Templates.
+    }
+
+    public boolean transferMoneyTo(Account account) {
+        return false;
+    }
 }
--- src/tests/AccountHolderTest.java
+++ src/tests/AccountHolderTest.java
@@ -4,35 +4,70 @@ import codes.Account;
 import codes.AccountHolder;
 import org.junit.Before;
 import org.junit.Test;
+import java.util.ArrayList;
+import java.util.List;
+import static junit.framework.Assert.*;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
 
 public class AccountHolderTest {
     private AccountHolder accountHolder;
-    private Map<Account,Integer> accountsOwned;
+    private List<Account> accountsOwned;
     private Account a1,a2,a3;
     private Integer i1,i2,i3;
+    private int amount$1000;
+    private int amount$200;
 
     @Before
     public void setUp(){
         accountHolder = new AccountHolder();
+        accountsOwned = new ArrayList<Account>();
         a1 = new Account();
         a2 = new Account();
         a3 = new Account();
-        i1 = new Integer(1000);
-        i2 = new Integer(2000);
-        i3 = new Integer(3000);
+        amount$1000 = 1000;
+        amount$200 = 200;
     }
 
     @Test
-    public void TestItShouldAddAccounts(){
-        accountsOwned = new LinkedHashMap<Account,Integer>();
-        accountsOwned.put(a1,i1);
-        accountsOwned.put(a2,i2);
-        accountsOwned.put(a3,i3);
+    public void testItShouldAddAccounts(){
+//        accountsOwned.add(a1);
+//        accountsOwned.add(a2);
+//        accountsOwned.add(a3);
+        assertTrue(accountsOwned.size() == 3);
     }
 
+    @Test
+    public void testItShouldGetCurrentBalanceOfAnAccount(){
+//        accountsOwned.add(a1);
+        accountHolder.addBalance(amount$1000);
+        assertTrue(accountHolder.getBalance() == amount$1000);
+    }
 
+    @Test
+    public void testItShouldGetCurrentBalanceAllAccount(){
+        accountHolder.addBalance(amount$1000);
+        accountHolder.addBalance(amount$1000);
+        accountHolder.addBalance(amount$1000);
+        assertEquals(accountHolder.getBalanceAllAccount(), amount$1000 *3);
+    }
+
+    @Test
+    public void testItShouldWithdrawMoney(){
+        accountHolder.addBalance(amount$1000);
+        assertTrue(accountHolder.setAmount(amount$200).withdrawMoney());
+        assertTrue(accountHolder.getBalance() == 800);
+        assertFalse(accountHolder.setAmount(amount$1000).withdrawMoney());
+        assertTrue(accountHolder.getBalance() == 800);
+    }
+
+    @Test
+    public void testItShouldTransferMoney(){
+        accountHolder.addBalance(amount$1000);
+        assertTrue(accountHolder.setAmount(amount$1000).transferMoneyTo(a2));
+        assertTrue(accountHolder.getBalance() == amount$1000 - amount$200);
+        assertTrue(accountHolder.getBalance() == amount$200);
+        assertFalse(accountHolder.setAmount(amount$1000).transferMoneyTo(a2));
+        assertTrue(accountHolder.getBalance() == amount$1000 - amount$200);
+    }
 
 }
